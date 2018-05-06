d7f5b144769fdd96d2f77863f07e569fc47033a7
Mervi Köninki <mervi.koninki@gmail.com>
2018-05-06 18:36:42 +0300

    Initial commit

---
+++ app/src/main/java/com/viola2013/popularmoviesapp/DetailsActivity.java
@@ -0,0 +1,74 @@
+package com.viola2013.popularmoviesapp;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+
+
+
+/**
+ * Created by Mervi on 2.5.2018.
+ */
+public class MovieDetailsActivity extends AppCompatActivity {
+    /**
+     * For logging purposes
+     */
+    private final String LOG_TAG = MovieDetailsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        TextView tvOriginalTitle = findViewById(R.id.textview_original_title);
+        ImageView ivPoster = findViewById(R.id.imageview_poster);
+        TextView tvOverView = findViewById(R.id.textview_overview);
+        TextView tvVoteAverage = findViewById(R.id.textview_vote_average);
+        TextView tvReleaseDate = findViewById(R.id.textview_release_date);
+
+        Intent intent = getIntent();
+        Movie movie = intent.getParcelableExtra(getString(R.string.parcel_movie));
+
+        tvOriginalTitle.setText(movie.getOriginalTitle());
+
+        Picasso.with(this)
+                .load(movie.getPosterPath())
+                .resize(getResources().getInteger(R.integer.tmdb_poster_w185_width),
+                        getResources().getInteger(R.integer.tmdb_poster_w185_height))
+                .error(R.drawable.not_found)
+                .placeholder(R.drawable.searching)
+                .into(ivPoster);
+
+        String overView = movie.getOverview();
+        if (overView == null) {
+            tvOverView.setTypeface(null, Typeface.ITALIC);
+            overView = getResources().getString(R.string.no_summary_found);
+        }
+        tvOverView.setText(overView);
+        tvVoteAverage.setText(movie.getDetailedVoteAverage());
+
+        // First get the release date from the object - to be used if something goes wrong with
+        // getting localized release date (catch).
+        String releaseDate = movie.getReleaseDate();
+        if(releaseDate != null) {
+            try {
+                releaseDate = DateTimeHelper.getLocalizedDate(this,
+                        releaseDate, movie.getDateFormat());
+            } catch (ParseException e) {
+                Log.e(LOG_TAG, "Error with parsing movie release date", e);
+            }
+        } else {
+            tvReleaseDate.setTypeface(null, Typeface.ITALIC);
+            releaseDate = getResources().getString(R.string.no_release_date_found);
+        }
+        tvReleaseDate.setText(releaseDate);
+    }
+}
---
+++ app/src/main/java/com/viola2013/popularmoviesapp/MainActivity.java
@@ -0,0 +1,8 @@
+package com.viola2013.popularmoviesapp;
+
+/**
+ * Created by Mervi on 2.5.2018.
+ */
+
+public class MainActivity {
+}
